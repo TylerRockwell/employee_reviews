6e40928eb837cd1c91b93073685dc28fadc2a81c
Tyler Rockwell <tyler.rockwell87@gmail.com>
2015-09-13 21:17:40 -0400

    Add NLP to assess reviews.

--- employee.rb
+++ employee.rb
@@ -14,4 +14,24 @@ class Employee
   def give_raise(amount: 0)
     @salary += amount
   end
+
+  def give_review(review)
+    @reviews << review
+  end
+
+  def assess_performance
+    # @satisfactory = false unless @reviews[-1].scan(/Zeke/) == []
+    # @satisfactory = true unless @reviews[-1].scan(/Xavier/) == []
+    good_terms = [/positive/i, /good/i, /\b(en)?courag(e[sd]?|ing)\b/i, /ease/i, /improvement/i, /quick(ly)?/i, /incredibl[ey]/i, /\bimpress[edving]?{2,3}/i]
+    bad_terms = [/\broom\bfor\bimprovement/i, /not/i, /less/i, /\bun[a-z]?{4,9}\b/i, /\b((inter)|e|(dis))?rupt[ivnge]{0,3}\b/i]
+    good_terms = Regexp.union(good_terms)
+    bad_terms = Regexp.union(bad_terms)
+    total_good_terms = []
+    total_bad_terms = []
+
+    total_good_terms = @reviews[-1].scan(good_terms)
+    total_bad_terms = @reviews[-1].scan(bad_terms)
+
+    total_good_terms.length > total_bad_terms.length ? (@satisfactory = true) : (@satisfactory = false)
+  end
 end
